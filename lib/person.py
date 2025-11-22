01dcd7777a077e1886596ecaf6c6af30e8fcc615
Peter Xervi <xervipeter@gmail.com>
2025-11-22 11:47:55 +0300

    done

--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,11 @@
 
 class Person:
     # Class body goes here
+    def talk(self):
+        print ("Hello World!")
+
+    def walk(self):
+        print ("The person is walking.")
 
     #Instance method definition
-    pass
+    
